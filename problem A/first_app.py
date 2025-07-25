ed3887b8e9742d7e31808a47d5d3b6f3517c0a72
ZakTechie <zakaria.ahmed6765@gmail.com>
2025-07-25 17:43:19 +0300

    upload

---
+++ problem A/first_app.py
@@ -0,0 +1,76 @@
+# print("Hello ")
+
+# Python indentation 
+
+
+# if 5 > 3 : 
+#     print("Five is greater than 3")
+
+
+# variables Rules:
+# varible must start with letter or underscore 
+# variable cannot start with number 
+# variable contain only (A-z , 0,9 and _)
+# cannot use reserved word 
+# cannot contain spaces
+
+# myvar = "h"
+# my_var= "hh"
+# 3my_var = "John"
+# my var = "Ali"
+# my-var = "Gamal"
+
+
+
+# Name Convention:
+# Camel Case:  myVarName = "a"
+# pascal case: MyVarName = "a"
+# snake case : my_var_name = "f" (prefered)
+
+
+# x = 5 
+# y = "7"
+# name = 'Ahmed'
+
+# print(type(y))
+# print( x + float(y))
+
+# Many values to Multiple varibales 
+# x , y , z = "a" , "b" , "c"
+# x = y = z = "a"
+
+# # unpack collection
+# fruits = ["apple" , "banana" , "khaled", "cherry"]
+# x , y , _ , z = fruits 
+
+# print(x , y , z) 
+
+
+# def func():
+#     global name 
+#     name = "Ahmed"
+#     print(name)
+
+# func()
+
+# print(name)
+# name = "Ali"
+
+
+# Data types: 
+# str 
+# int , float , complex
+# list , tuple , range
+# dict
+# set , frozenset
+# bool 
+# bytes , bytearray , memoryview 
+# NoneType
+
+
+z = None
+print(type(z))
+
+
+name = input("Enter yourname: ")
+print(name)
